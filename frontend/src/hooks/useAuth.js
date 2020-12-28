04e26ade5e800e2a6928cfe9c129ebe466010a37
danielkanangila <daniel.kanangila@gmail.com>
2020-12-28 14:36:25 -0500

    DRY useAuth hook

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 import { useLocalStorage } from "./useLocalStorage";
 import useApi from "./useApi";
 import authApiFunc from "./../api/auth";
+import { handleFormSubmission } from "../utils";
 
 /**
  * Authentication hook
@@ -10,75 +11,44 @@ const useAuth = (authFunc) => {
   const [user, setUser] = useLocalStorage("user", null);
   const authApi = useApi(authFunc);
 
-  const signup = async (
-    data,
-    { setErrors, setStatus, resetForm },
-    onSuccess
-  ) => {
+  const signup = async (data, formHandler, onSuccess) => {
     if (authFunc.name !== authApiFunc.register.name)
       throw Error(
         `Invalid authentication api function [${authFunc.name}]. Should be [${authApiFunc.register.name}]`
       );
-    // set form status to loading to true
-    setStatus({ loading: true });
-    const response = await authApi.request({ ...data, username: data.email });
-    // handle response error if any
-    if (!response.ok) {
-      console.log(response);
-      let error = response.data;
+    const _request = async () =>
+      await authApi.request({ ...data, username: data.email });
+
+    const _handleError = (error, setErrors, setStatus) => {
       // change the email message
-      if (
-        "email" in response.data &&
-        response.data.email.join(" ").includes("must be unique")
-      )
+      if ("email" in error && error.email.join(" ").includes("must be unique"))
         error.email = "This email is already taken.";
       // set the errors to the form errors
       setErrors(error);
       // set form loading status to false
       setStatus({ loading: false });
       return;
-    }
-    // handle response success
-    // set user to the local storage
-    setUser(response.data);
-    // set form loading status to false
-    setStatus({ loading: false });
-    // reset the form
-    resetForm();
-    // call the callback function
-    onSuccess();
+    };
+    // call the form handler function to process the form submission
+    await handleFormSubmission(
+      _request,
+      formHandler,
+      onSuccess,
+      setUser,
+      _handleError
+    );
   };
 
-  const login = async (
-    data,
-    { setErrors, setStatus, resetForm },
-    onSuccess
-  ) => {
+  const login = async (data, formHandler, onSuccess) => {
     if (authFunc.name !== authApiFunc.login.name)
       throw Error(
         `Invalid authentication api function [${authFunc.name}]. Should be [${authApiFunc.login.name}]`
       );
-    // set form status to loading to true
-    setStatus({ loading: true });
-    const response = await authApi.request(data.username, data.password);
-    // handle response error if any
-    if (!response.ok) {
-      console.log(response);
-      // set the errors to the form errors
-      setErrors(response.data);
-      // set form loading status to false
-      setStatus({ loading: false });
-      return;
-    }
-    // handle response success
-    // set user to the local storage
-    setUser(response.data);
-    // set form loading status to false
-    setStatus({ loading: false });
-    // reset the form
-    resetForm();
-    // call the callback function
-    onSuccess();
+    // request helper function
+    const _request = async () =>
+      await authApi.request(data.username, data.password);
+    // call the form handler function to process the form submission
+    await handleFormSubmission(_request, formHandler, onSuccess, setUser);
   };
 
   const logout = () => localStorage.removeItem("user");
---
+++ frontend/src/utils/index.js
@@ -0,0 +1,33 @@
+export const handleFormSubmission = async (
+  request,
+  formHandler,
+  onSuccess,
+  setUser,
+  onError
+) => {
+  const { setErrors, setStatus, resetForm } = formHandler;
+
+  // set form status to loading to true
+  setStatus({ loading: true });
+  const response = await request();
+  // handle response error if any
+  if (!response.ok) {
+    console.log(response);
+    // call error handler if defined
+    if (onError) return onError(response.data, setErrors, setStatus);
+    // set the errors to the form errors
+    setErrors(response.data);
+    // set form loading status to false
+    setStatus({ loading: false });
+    return;
+  }
+  // handle response success
+  // set user to the local storage
+  setUser(response.data);
+  // set form loading status to false
+  setStatus({ loading: false });
+  // reset the form
+  resetForm();
+  // call the callback function
+  onSuccess();
+};
