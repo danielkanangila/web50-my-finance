4e41607f43c92ea0059c09792ac6ad21e0383cd0
danielkanangila <daniel.kanangila@gmail.com>
2020-12-29 11:48:07 -0500

    added SecureRoute component to secure route that require authentication

---
+++ frontend/src/components/SecureRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+import { useLocalStorage } from "./../hooks/useLocalStorage";
+
+const SecureRoute = ({ component: Component, ...restOfProps }) => {
+  const [user] = useLocalStorage("user", null);
+
+  return (
+    <Route
+      {...restOfProps}
+      render={(props) => {
+        if (user) return <Component {...props} />;
+        return <Redirect to="/" />;
+      }}
+    />
+  );
+};
+
+export default SecureRoute;
