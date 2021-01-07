a5f16b797763d45b6ab20cfd0259ce9b7c6a450f
danielkanangila <daniel.kanangila@gmail.com>
2021-01-07 13:18:57 -0500

    added plaid api urls

--- frontend/src/api/plaid.js
+++ frontend/src/api/plaid.js
@@ -2,10 +2,18 @@ import client from "./client";
 
 const createLinkToken = () => client.post("/create_link_token", {});
 const setAccessToken = (data) => client.post("/set_access_token", data);
+const getAccounts = (userId) => client.get(`/users/${userId}/accounts`);
+const getAccountTransactions = (userId, accountId) =>
+  client.get(`/users/${userId}/accounts/${accountId}/transactions`);
+const getTransactions = (userId) => client.get(`/users/${userId}/transactions`);
 
 const plaid = {
   createLinkToken,
   setAccessToken,
+  getAccounts,
+  getTransactions,
+  getAccountTransactions,
+  getTransactions,
 };
 
 export default plaid;
